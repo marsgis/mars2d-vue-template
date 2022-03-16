3310aaa60f7ae86bdfaac8aee7fde95f962faa85
wanghao <muyao1987@qq.com>
2022-03-16 15:42:27 +0800

    v3.0.12

--- mars2d-vue3-vite/src/main.ts
+++ mars2d-vue3-vite/src/main.ts
@@ -1,6 +1,8 @@
 
-import "leaflet/dist/leaflet.css";
-import "mars2d/dist/mars2d.css";
+import "leaflet/dist/leaflet.css"
+import "leaflet"
+import "mars2d/dist/mars2d.css"
+import "mars2d"
 
 import { createApp } from 'vue'
 import App from './App.vue'
